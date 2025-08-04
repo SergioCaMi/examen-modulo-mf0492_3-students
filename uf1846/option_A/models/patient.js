13b4933514ebf68e30682e1be66cf8f5a4be9ccd
SergioCaMi <sergiocami84@gmail.com>
2025-08-04 17:53:55 +0200

    UF1846B

--- uf1846/option_A/models/patient.js
+++ uf1846/option_A/models/patient.js
@@ -1,25 +1,26 @@
-const { Schema, model, Types } = require('mongoose');
+const { Schema, model, Types } = require("mongoose");
 
 const patientSchema = new Schema({
-    ssn: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    birthDate: {
-        type: Date,
-        required: true
-    }
+  ssn: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^[0-6][0-9][ -]?[0-9]{8}[ -]?[0-9]{2}$/, "El campo SSN no tiene un formato válido"],
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  birthDate: {
+    type: Date,
+    required: true,
+  },
 });
 
-const Patient = model('patient', patientSchema);
+const Patient = model("patient", patientSchema);
 
 module.exports = Patient;
